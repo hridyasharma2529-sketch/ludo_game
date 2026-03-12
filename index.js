e362ec582a769f9b8b9ae668b7dcc52f7a8c8b13
hridyasharma2529-sketch <hridyasharma2529@gmail.com>
2026-03-13 03:07:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+randomNumber1=Math.floor(Math.random()*6)+1;
+
+img_left="images/dice"+randomNumber1+".png";
+document.querySelector(".img1").setAttribute("src",img_left);
+
+randomNumber2=Math.floor(Math.random()*6)+1;
+
+img_right="images/dice"+randomNumber2+".png";
+document.querySelector(".img2").setAttribute("src",img_right);
+
+if (randomNumber1>randomNumber2){
+    document.getElementsByTagName("h1")[0].innerHTML="Player 1 wins!"
+
+}
+if(randomNumber1<randomNumber2){
+     document.getElementsByTagName("h1")[0].innerHTML="Player 2 wins!"
+}
+if(randomNumber1==randomNumber2){
+     document.getElementsByTagName("h1")[0].innerHTML="Draw!"
+}
